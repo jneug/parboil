c44bc5a8b018a8b0525bb5564f39a798d54ce565
Jonas Neugebauer <jonas@neugebauer.cc>
2021-03-04 20:45:41 +0100

    Dev requirements

--- test/test_boil.py
+++ test/test_boil.py
@@ -1,12 +1,15 @@
 from click.testing import CliRunner
-from ..parboil.parboil import boil
-from ..parboil.version import __version__
+from parboil.parboil import boil
+from parboil.version import __version__
 
 def test_boil():
-  runner = CliRunner()
-  result = runner.invoke(boil)
-  assert result.exit_code == 0
-  
-  result = runner.invoke(boil, ['--version'])
-  assert result.exit_code == 0
-  assert __version__ in result.output
+	runner = CliRunner()
+	result = runner.invoke(boil)
+	assert result.exit_code == 0
+
+	result = runner.invoke(boil, ['--version'])
+	assert result.exit_code == 0
+	assert __version__ in result.output
+
+	result = runner.invoke(boil, ['unknown-command'])
+	assert result.exit_code == 2

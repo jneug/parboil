f58e4c60cba1994f00bde9ed1fd0a3f09551f097
Jonas Neugebauer <jneug@users.noreply.github.com>
2021-10-17 18:09:49 +0200

    Improved basic tests

--- tests/test_boil.py
+++ tests/test_boil.py
@@ -1,24 +1,44 @@
+"""Testing the basic boil command"""
+
 from click.testing import CliRunner
 from parboil.parboil import boil
 from parboil.version import __version__
 
 
-def test_boil():
-    runner = CliRunner()
-    result = runner.invoke(boil)
-    assert result.exit_code == 0
+def test_version():
+    assert __version__ == "0.7.7"
 
-    # Version information
-    result = runner.invoke(boil, ["--version"])
-    assert result.exit_code == 0
-    assert __version__ in result.output
 
+def test_boil_version(boil_runner):
+    result = boil_runner("--version")
+    assert result.exit_code == 0
+    assert f"boil, version {__version__}" == result.output
+
+    
+def test_boil_unknown(boil_runner):
     # Unknwon command
-    result = runner.invoke(boil, ["unknown-command"])
+    result = boil_runner.invoke("unknown-command")
     assert result.exit_code == 2
-    assert "No such command 'unknown-command'"
+    assert "Error: No such command 'unknown-command'." in result.output
 
     # Missing command
     result = runner.invoke(boil, ["--tpldir", "SOME/DIR"])
     assert result.exit_code == 2
-    assert "Missing command" in result.output
+    assert "Error: Missing command." in result.output
+
+def test_boil_help(boil_runner):
+    usage_str = "Usage: boil [OPTIONS] COMMAND [ARGS]..."
+    
+    # invoking without args should show help
+    result = boil_runner.invoke()
+    assert result.exit_code == 0
+    assert result.output.startswith(usage_str)
+    assert "Options:" in result.output
+    assert "Commands:" in result.output
+
+    # help message
+    result = boil_runner.invoke("--help")
+    assert result.exit_code == 0
+    assert result.output.startswith(usage_str)
+    assert "Options:" in result.output
+    assert "Commands:" in result.output

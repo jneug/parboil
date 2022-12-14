58686011cfcb30796cc38bfbd1a310a7f82c58cb
J. Neugebauer <ngb@helmholtz-bi.de>
2022-12-14 10:39:12 +0100

    Removed superfluous main function

--- src/parboil/settings.py
+++ src/parboil/settings.py
@@ -56,13 +56,3 @@ LOGGING_CONFIG = {
     },
     "root": {"level": "INFO", "handlers": ["logfile"]},
 }
-
-if __name__ == "__main__":
-    import tomli
-    from rich import inspect
-
-    with open(
-        "/Users/jneug/Projekte/Skripte/parboil/tests/templates/docs/parboil.toml", "rb"
-    ) as toml:
-        config = tomli.load(toml)
-        inspect(config)
